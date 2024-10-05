39d37e9e668935f273c5bb907aed2e5c4dee3324
Anil <anil6996f@gmail.com>
2024-10-05 13:39:41 +0530

    implemented admin logout functionality

--- frontend/src/store/auth-slice/index.js
+++ frontend/src/store/auth-slice/index.js
@@ -23,6 +23,14 @@ export const loginUser = createAsyncThunk('/auth/login', async (formData) => {
     return response.data;
 });
 
+export const logoutUser = createAsyncThunk('/auth/logout', async () => {
+    const response = await axios.post('http://localhost:5000/api/auth/logout', {}, { //have to keep empty obj 
+        withCredentials: true
+    });
+
+    return response.data;
+});
+
 export const checkAuth = createAsyncThunk('/auth/checkauth', async () => {
     const response = await axios.get('http://localhost:5000/api/auth/check-auth', {
         withCredentials: true,
@@ -72,6 +80,10 @@ const authSlice = createSlice({
             state.user = action.payload.success ? action.payload.user : null;
             state.isAuthenticated = action.payload.success;
         }).addCase(checkAuth.rejected, (state) => {
+            state.isLoading = false;
+            state.user = null;
+            state.isAuthenticated = false;
+        }).addCase(logoutUser.fulfilled, (state) => {
             state.isLoading = false;
             state.user = null;
             state.isAuthenticated = false;
